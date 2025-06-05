6488c113e5a11dee6fc16baa9820d3b7c844fcd9
devansht108 <tiwaridevansh21@gmail.com>
2025-06-05 17:54:40 +0530

    added variable

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title='Welcome to the new blog';
+
   return(
     <div className="App">
       <div className="content">
-        <h1>App content</h1>
+        <h1>{title}</h1>
       </div>
     </div>
   );
