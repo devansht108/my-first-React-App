b1631cc4e4d49446def2c2e931e7b4ac2e101a24
devansht108 <tiwaridevansh21@gmail.com>
2025-06-06 15:44:52 +0530

    Added site

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 function App() {
   const title='Welcome to the new blog';
+  const link="http://www.google.com"
 
   return(
     <div className="App">
       <div className="content">
         <h1>{title}</h1>
         <p>{Math.random()*20}</p>
+        <a href={link}>Google</a>
       </div>
     </div>
   );
