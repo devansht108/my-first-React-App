8d9d005a7891458db496b2f3724a8438d9666e15
devansht108 <tiwaridevansh21@gmail.com>
2025-06-16 01:39:53 +0530

    feat: add Home component with greeting buttons fix: render Home component correctly in App.js refactor: move Home component to components folder
    - Created Home.js with two buttons
    - One button logs a message on click
    - Another button logs a personalized greeting
    - Imported and rendered Home component in App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import logo from './logo.svg';
-import './App.css';
+import Home from './Home';
 
 function App() {
-  const title='Welcome to the new blog';
-  const link="http://www.google.com"
+  const title = 'Welcome to the new blog';
+  const link = "http://www.google.com";
 
-  return(
+  return (
     <div className="App">
       <div className="content">
         <h1>{title}</h1>
-        <p>{Math.random()*20}</p>
+        <p>{Math.random() * 20}</p>
         <a href={link}>Google</a>
+
+        {/* ✅ Add this line to render Home component */}
+        <Home />
       </div>
     </div>
   );
